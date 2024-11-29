c322a1c9811139fbc620fad71a06f7614e11cd84
titi <toto@gmail.com>
2024-11-29 08:51:50 +0100

    02

---
+++ test5.js
@@ -0,0 +1,11 @@
+//chaine de caractère
+var MavariableString = "toto";
+//nombre
+var MavariableNumerique = window.innerWidth;
+
+if (MavariableString == "toto"&& MavariableNumerique !=300|| MavariableNumerique <=200){
+    alert("Inférieur a 300px ");
+}else{
+    alert("supérieur a 300px");
+}
+
